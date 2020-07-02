d826fba34161e123f3dd52ce7caf139d88866fbd
iccanobif <iccanobif@protonmail.com>
2020-07-02 23:14:25 +0200

    display component

---
+++ src/Dictionary.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function Dictionary(props) {
+  const { dictionaryQueryResults } = props;
+
+  return (
+    <ul>
+      {dictionaryQueryResults.map((w, i) => (
+        <React.Fragment key={i}>{Word(w, i)}</React.Fragment>
+      ))}
+    </ul>
+  );
+}
+
+function Word(wordData) {
+  return (
+    <>
+      {wordData.dictionaryEntries.map((entry, i) => (
+        <li key={i} style={{ listStyleType: "none" }}>
+          {DictionaryEntry(entry)}
+        </li>
+      ))}
+    </>
+  );
+}
+
+function DictionaryEntry(entry) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsExpanded(!isExpanded)}>
+        {isExpanded ? "-" : "+"}
+      </button>
+      {entry.lemmas.map((l) => l)}
+      {!isExpanded ? (
+        <></>
+      ) : (
+        <>
+          <div>{entry.accents}</div>
+          {entry.japaneseGlosses.concat(entry.englishGlosses).map((gloss, i) => (
+            <div key={i}>
+                {gloss}
+            </div>
+          ))}
+        </>
+      )}
+    </>
+  );
+}
