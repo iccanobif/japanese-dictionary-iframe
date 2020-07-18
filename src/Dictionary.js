945c57cd7825762d291909daeb72f9b1f50c3ae6
iccanobif <iccanobif@protonmail.com>
2020-07-18 15:39:02 +0200

    meaningless commit to force a new deploy on heroku

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,21 @@ import React, { useState } from "react";
 
 export default function Dictionary(props) {
   const { dictionaryQueryResults, isQueryRunning } = props;
-  if (isQueryRunning)
-    return <>読込中</>
+  if (isQueryRunning) return <>読込中</>;
 
   return (
-      <ul>
-        {dictionaryQueryResults.map((w, i) => (
-          <React.Fragment key={i}><Word word={w} /></React.Fragment>
-        ))}
-      </ul>
+    <ul>
+      {dictionaryQueryResults.map((w, i) => (
+        <React.Fragment key={i}>
+          <Word word={w} />
+        </React.Fragment>
+      ))}
+    </ul>
   );
 }
 
 function Word(props) {
-  const word = props.word
+  const word = props.word;
   return (
     <>
       {word.dictionaryEntries.map((entry, i) => (
@@ -28,10 +29,10 @@ function Word(props) {
 }
 
 function DictionaryEntry(props) {
-  const entry = props.entry
+  const entry = props.entry;
   const [isExpanded, setIsExpanded] = useState(false);
 
-  return ( 
+  return (
     <>
       <button onClick={() => setIsExpanded(!isExpanded)}>
         {isExpanded ? "-" : "+"}
